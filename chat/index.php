7d4bf7fef86b8578eb67cec2af4cf423b330a755
mkohei <baberugaberiimeronnv5@gmail.com>
2016-12-26 04:17:47 +0900

    Update chat
    Update chat関数の実装

--- chat/index.php
+++ chat/index.php
@@ -18,6 +18,17 @@ $KEY_CONTENT = "content";
 # private ID
 $KEY_LOCK = "lock";
 
+// DB SELECT RESULT KEY : update chat
+$KEY_CHAT_ID = "chatId";
+# room ID
+$KEY_USER_ID = "userId";
+# content
+$KEY_CHAT_TIME = "chatTime";
+
+// RETURN JSON KEY : update cht
+$KEY_CHATS = "chats";
+# last time : 
+
 
 // Processing
 $req = $_SERVER["REQUEST_METHOD"];
@@ -59,7 +70,64 @@ if ($req == "GET") {
 
 // functions
 // update chat (get chat)
-function get_chat($privateId, $roomId, $lastTime) {
+function get_chat($privateId, $roomId, $lastTime) {    
+    // need private ID and room ID
+    if (empty($privateId) or empty($roomId)) {
+        echo badreq();
+        die();
+    }
+
+    global $DNS, $USER, $PW;
+
+    try {
+        $pdo = new PDO($DNS, $USER, $PW);
+        if ($pdo == null) {
+            echo servererr();
+            die();
+        }
+
+        // Select chat
+        // SQL
+        $sql = "SELECT
+            a.chatId, a.roomId, a.userId, a.content, a.chatTime
+            FROM chat a, user b
+            WHERE a.userId = b.userId
+            AND a.roomId = :roomId AND b.privateId = :privateId
+            AND a.chatTime >= :lastTime";
+        $stmt = $pdo->prepare($sql);
+        $params = array (
+            ':roomId' => $roomId,
+            ':privateId' => $privateId,
+            ':lastTime' => $lastTime
+        );
+        $stmt->execute($params);
+        $result = $stmt->fetchAll();
+        $chats = array ();
+        global $KEY_CHAT_ID, $KEY_ROOM_ID, $KEY_USER_ID, $KEY_CONTENT, $KEY_CHAT_TIME, $KEY_LAST_TIME, $TIME_FORMAT, $KEY_CHATS;
+        foreach ($result as $val) {
+            $chat = array (
+                $KEY_CHAT_ID => (int)$val[$KEY_CHAT_ID],
+                $KEY_ROOM_ID => (int)$val[$KEY_ROOM_ID],
+                $KEY_USER_ID => (int)$val[$KEY_USER_ID],
+                $KEY_CONTENT => $val[$KEY_CONTENT],
+                $KEY_CHAT_TIME => $val[$KEY_CHAT_TIME]
+            );
+            $chats[] = $chat;
+        }
+        $lastTime = date($TIME_FORMAT);
+        return json_encode(
+            array (
+                $KEY_CHATS => $chats,
+                $KEY_LAST_TIME => $lastTime
+            )
+        );
+        
+
+    } catch (Exception $e) {
+        $pdo = null;
+        echo servererr();
+        die();
+    }
 
 }
 
